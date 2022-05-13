e5e1e1cb7b0bdf48ae701c31ae10d697ddd9250a
carvalhocaio <caiocarvalho.py@gmail.com>
2022-05-13 00:43:53 -0400

    it e xit

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,16 +1,14 @@
 require 'calculator'
 
 describe Calculator do
-  subject(:calc) { described_class.new }
-
   context '#sum' do
     it 'with positive numbers' do
-      result = calc.sum(5, 7)
+      result = subject.sum(5, 7)
       expect(result).to eq(12)
     end
 
-    it 'with negative numbers' do
-      result = calc.sum(-5, -7)
+    xit 'with negative numbers' do
+      result = subject.sum(-5, -7)
       expect(result).to eq(-12)
     end
   end
