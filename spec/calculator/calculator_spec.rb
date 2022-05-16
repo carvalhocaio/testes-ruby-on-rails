353b67ba5c5570f8413855ab232a5f5f159bd4e7
carvalhocaio <caiocarvalho.py@gmail.com>
2022-05-16 15:10:45 -0400

    matchers de igualdade

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,13 +1,14 @@
 require 'calculator'
 
-describe Calculator do
+describe Calculator, 'Test about calculator class' do
   context '#sum' do
     it 'with positive numbers' do
       result = subject.sum(5, 7)
       expect(result).to eq(12)
+      expect(result).not_to eq(11)
     end
 
-    xit 'with negative numbers' do
+    it 'with negative numbers' do
       result = subject.sum(-5, -7)
       expect(result).to eq(-12)
     end
