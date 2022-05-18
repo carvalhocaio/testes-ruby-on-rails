efbaab0ebfb945814bb2819f18ed74e546d4d359
carvalhocaio <caiocarvalho.py@gmail.com>
2022-05-18 14:42:28 -0400

    matchers de erros

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,8 @@ class Calculator
   def sum(number1 = 0, number2 = 0)
     number1 + number2
   end
+
+  def div(a, b)
+    a / b
+  end
 end
--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,6 +1,15 @@
 require 'calculator'
 
 describe Calculator, 'Test about calculator class' do
+  context '#div' do
+    it 'divide by zero' do
+      expect { subject.div(3, 0) }.to raise_error(ZeroDivisionError)
+      expect { subject.div(3, 0) }.to raise_error('divided by 0')
+      expect { subject.div(3, 0) }.to raise_error(ZeroDivisionError, 'divided by 0')
+      expect { subject.div(3, 0) }.to raise_error(/divided/)
+    end
+  end
+
   context '#sum' do
     it 'with positive numbers' do
       result = subject.sum(5, 7)
