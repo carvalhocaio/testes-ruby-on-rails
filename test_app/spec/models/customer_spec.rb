a62abb5cc0f6351a912edb7b0cb54dbe41322324
carvalhocaio <caiocarvalho.py@gmail.com>
2022-06-14 23:02:19 -0400

    heranca

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to eq('Mrs. Leia Organa')
   end
 
+  it '#customer_vip' do
+    customer = create(:customer_vip)
+    expect(customer.vip).to eq(true)
+    expect(customer.days_to_pay).to eq(30)
+  end
+
+  it '#customer_default' do
+    customer = create(:customer_default)
+    expect(customer.vip).to eq(false)
+    expect(customer.days_to_pay).to eq(15)
+  end
+
   it '#full_name - faker' do
     customer = create(:user) # ou create(:customer)
     expect(customer.full_name).to start_with('Mrs.')
