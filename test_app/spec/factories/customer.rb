4cbc2b829ce51c55a39fc9227ae78558641797ae
carvalhocaio <caiocarvalho.py@gmail.com>
2022-06-14 22:27:14 -0400

    conhecendo o factorybot

---
+++ test_app/spec/factories/customer.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :customer do
+    name { 'Julie Bowen' }
+    email { 'julie.bowen@mail.com' }
+  end
+end
--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  # fixtures :customers
-  fixtures :all
-
   it 'Create a Customer' do
-    customer = customers(:julie)
+    customer = create(:customer)
     expect(customer.full_name).to eq('Mrs. Julie Bowen')
   end
 end
+
+# factoryBot
+# build -> not save
+# create -> save
