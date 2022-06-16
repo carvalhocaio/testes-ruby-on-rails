a162d29a50d972e1cf520dea4b2df7c2a7c65472
carvalhocaio <caiocarvalho.py@gmail.com>
2022-06-15 22:52:32 -0400

    traits

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -43,6 +43,28 @@ RSpec.describe Customer, type: :model do
     expect(customer.name.upcase).to eq(customer.name)
   end
 
+  it '#customer_male' do
+    customer = create(:customer_male)
+    expect(customer.gender).to eq('M')
+  end
+
+  it '#customer_male_vip' do
+    customer = create(:customer_male_vip)
+    expect(customer.gender).to eq('M')
+    expect(customer.vip).to eq(true)
+  end
+
+  it '#customer_female' do
+    customer = create(:customer_female)
+    expect(customer.gender).to eq('F')
+  end
+
+  it '#customer_female_default' do
+    customer = create(:customer_female_default)
+    expect(customer.gender).to eq('F')
+    expect(customer.vip).to eq(false)
+  end
+
   it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
 end
 
