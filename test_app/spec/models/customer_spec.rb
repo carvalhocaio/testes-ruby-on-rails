341f744269074eee050860ab5ad5d485e73975cd
carvalhocaio <caiocarvalho.py@gmail.com>
2022-06-27 12:47:17 -0400

    time helpers

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -68,6 +68,15 @@ RSpec.describe Customer, type: :model do
     expect(customer.vip).to eq(false)
   end
 
+  it 'travel_to' do
+    travel_to Time.zone.local(2004, 11, 24, 0o1, 0o4, 44) do
+      @customer = create(:customer_vip)
+    end
+
+    # expect(@customer.created_at).to eq(Time.new(2004, 11, 24, 0o1, 0o4, 44, 'UTC'))
+    expect(@customer.created_at).to be < Time.now
+  end
+
   it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
 end
 
