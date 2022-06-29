b32cfb7fd3c821f53f9a69225de4557519c491d2
carvalhocaio <caiocarvalho.py@gmail.com>
2022-06-29 15:48:02 -0400

    configrando o screenshot

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'Customers', type: :feature do
+RSpec.feature 'Customers', type: :feature, js: true do
   it 'visit index page' do
     visit(customers_path)
     page.save_screenshot('screenshot.png')
